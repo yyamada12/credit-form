7b041e4d3def629b96bbed52e592b2e034661864
yyamada12 <12yacropolisy@gmail.com>
2020-02-22 23:05:35 +0900

    add cardNumber state

--- src/App.tsx
+++ src/App.tsx
@@ -6,13 +6,28 @@ import TextField from "@material-ui/core/TextField";
 import Select from "@material-ui/core/Select";
 import Button from "@material-ui/core/Button";
 
-function App() {
-  const containerStyle = {
+interface AppPropsInterface {}
+
+interface AppStateInterface {
+  cardNumber: string;
+}
+
+class App extends React.Component<AppPropsInterface, AppStateInterface> {
+  constructor(props: AppPropsInterface) {
+    super(props);
+    this.state = {
+      cardNumber: ""
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  containerStyle = {
     backgroundColor: "#cfe8fc",
     height: "100vh",
     overflow: "hidden"
   };
-  const creditCardStyle = {
+  creditCardStyle = {
     backgroundImage: "url('image/1.jpeg')",
     backgroundSize: "cover",
     borderRadius: "10px",
@@ -20,86 +35,100 @@ function App() {
     width: "400px",
     margin: "-700px auto"
   };
-  const paperStyle = {
+  paperStyle = {
     backgroundColor: "white",
     margin: "100px auto",
     height: "550px",
     overflow: "hidden"
   };
-  const formContainerStyle = {
+  formContainerStyle = {
     width: "80%",
     margin: "200px auto"
   };
-  const cardNumberFormStyle = {
+  cardNumberFormStyle = {
     width: "100%",
     margin: "10px 0"
   };
-  const cardNameFormStyle = {
+  cardNameFormStyle = {
     width: "100%",
     margin: "10px 0"
   };
-  const expireMonthSelectStyle = {
+  expireMonthSelectStyle = {
     marginTop: "10px",
     marginRight: "10px"
   };
-  const expireYearSelectStyle = {
+  expireYearSelectStyle = {
     marginTop: "10px",
     marginRight: "10px"
   };
-  const CWFormStyle = {
+  CWFormStyle = {
     marginTop: "10px",
     marginRight: "10px"
   };
-  const buttonStyle = {
+  buttonStyle = {
     marginTop: "30px",
     width: "100%",
     height: "50px"
   };
-
-  return (
-    <div className="App">
-      <CssBaseline />
-      <Container fixed style={containerStyle}>
-        <Paper style={paperStyle}>
-          <div style={formContainerStyle}>
-            <TextField
-              required
-              label="Card Number"
-              variant="outlined"
-              style={cardNumberFormStyle}
-            />
-            <TextField
-              required
-              label="Card Name"
-              variant="outlined"
-              style={cardNameFormStyle}
-            />
-            <Select
-              label="Expiration Month"
-              variant="outlined"
-              style={expireMonthSelectStyle}
-            />
-            <Select
-              required
-              label="Expiration Year"
-              variant="outlined"
-              style={expireYearSelectStyle}
-            />
-            <TextField
-              required
-              label="CW"
-              variant="outlined"
-              style={CWFormStyle}
-            />
-            <Button variant="contained" color="primary" style={buttonStyle}>
-              Submit
-            </Button>
-          </div>
-        </Paper>
-        <Paper elevation={8} style={creditCardStyle} />
-      </Container>
-    </div>
-  );
+  handleChange(event: React.ChangeEvent<HTMLTextAreaElement>) {
+    const target = event.target;
+    this.setState(() => {
+      return { ...this.state, [target.name]: target.value };
+    });
+  }
+  render() {
+    return (
+      <div className="App">
+        <CssBaseline />
+        <Container fixed style={this.containerStyle}>
+          <Paper style={this.paperStyle}>
+            <div style={this.formContainerStyle}>
+              <TextField
+                required
+                name="cardNumber"
+                label="Card Number"
+                variant="outlined"
+                style={this.cardNumberFormStyle}
+                value={this.state.cardNumber}
+                onChange={this.handleChange}
+              />
+              <TextField
+                required
+                label="Card Name"
+                variant="outlined"
+                style={this.cardNameFormStyle}
+              />
+              <Select
+                label="Expiration Month"
+                variant="outlined"
+                style={this.expireMonthSelectStyle}
+              />
+              <Select
+                required
+                label="Expiration Year"
+                variant="outlined"
+                style={this.expireYearSelectStyle}
+              />
+              <TextField
+                required
+                label="CW"
+                variant="outlined"
+                style={this.CWFormStyle}
+              />
+              <Button
+                variant="contained"
+                color="primary"
+                style={this.buttonStyle}
+              >
+                Submit
+              </Button>
+            </div>
+          </Paper>
+          <Paper elevation={8} style={this.creditCardStyle} />
+        </Container>
+      </div>
+    );
+  }
 }
 
 export default App;
